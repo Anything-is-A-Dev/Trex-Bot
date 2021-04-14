cb00803aa5f281a996e258ca69b70e3bbd46b416
Anything <68682125+Anything-is-A-Dev@users.noreply.github.com>
2021-04-14 15:16:30 +0530

    da t rex lul
    iz da trex lul

--- Trex Bot/trex dystryoyer.py
+++ Trex Bot/trex dystryoyer.py
@@ -3,42 +3,50 @@ import time
 import keyboard
 
 
-
+# these are the width,height,x,y values of the place the bot patrols to find cactus, and the delay time
+width = 120
+height = 150
+x = 450
+y = 188
+slep = 0.2
 
 # This is a space function that preses space
 def space():
     keyboard.press("space")
-    time.sleep(0.01)
+    time.sleep(0.1)
     keyboard.release("space")
 
-# these are the width,height,x,y values of the place the bot patrols to find cactus
-width = 180
-height = 150
-x = 540
-y = 188
+
 
 # This is the main loop
 while keyboard.is_pressed("q") == False:
    if pyautogui.locateOnScreen('cactus.png', region=(x, y, width , height), confidence= 0.8,) != None:
-       time.sleep(0.3) # adds a delay of 0.3 secs
+       time.sleep(slep) # adds a delay of 0.3 secs
        space()
 
 
 
+
    elif pyautogui.locateOnScreen('smol_cactus.png',region=(x, y, width , height), confidence=0.8) != None:
-       time.sleep(0.3)# adds a delay of 0.3 secs
+       time.sleep(slep)
        space()
 
 
 
 
    elif pyautogui.locateOnScreen('double_cactus.png',region=(x, y , width , height), confidence=0.8) != None:
-       time.sleep(0.3)# adds a delay of 0.3 secs
+       time.sleep(slep)
        space()
+
 
 
    elif pyautogui.locateOnScreen('triple.png', region=(x, y, width, height), confidence=0.8) != None:
-       time.sleep(0.2)# adds a delay of 0.3 secs
+       time.sleep(slep)# adds a delay of 0.3 secs
        space()
+
+
+
+
+
 
 
